f3e98e00d93a9f064fa7577f39ecd7631a61f2bf
ersano syaifurrahman <ersano.syaifurrahman98@gmail.com>
2024-01-02 22:36:58 +0700

    first push

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+<script>
+
+   function hitungLuas()
+     alas = document.getElementById("alas").value;
+     tinggi = document.getElementById("tinggi").value;
+     luas = (alas*tinggi)*0.5;
+     document.getElementById("luas").value=luas;
+     
+</script>
