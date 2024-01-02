56f6265ad09ab312a5c1e4cbbf846ad84ad7228b
ersano syaifurrahman <ersano.syaifurrahman98@gmail.com>
2024-01-03 00:23:42 +0700

    add pict and js

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 <script>
 
-   function hitungLuas()
-     alas = document.getElementById("alas").value;
-     tinggi = document.getElementById("tinggi").value;
-     luas = (alas*tinggi)*0.5;
-     document.getElementById("luas").value=luas;
+  function hitungLuas()
+  alas = document.getElementById("alas").value;
+  tinggi = document.getElementById("tinggi").value;
+  luas = (alas*tinggi)*0.5;
+  document.getElementById("luas").value=luas;
      
 </script>
